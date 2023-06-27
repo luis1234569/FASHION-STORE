5958152c536b8f71206796048923e676f3bf8df3
luis1234569 <lav.ruiz@yavirac.edu.ec>
2023-06-27 13:03:44 -0500

    auth

--- lib/service/auth-service.dart
+++ lib/service/auth-service.dart
@@ -76,19 +76,3 @@ Future<bool> login(String email, String password) async {
 }
 
 
-// Future<UserCredential> signInWithGoogleMen() async {
-//   // Trigger the authentication flow
-//   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-//   // Obtain the auth details from the request
-//   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-//   // Create a new credential
-//   final credential = GoogleAuthProvider.credential(
-//     accessToken: googleAuth?.accessToken,
-//     idToken: googleAuth?.idToken,
-//   );
-
-//   // Once signed in, return the UserCredential
-//   return await FirebaseAuth.instance.signInWithCredential(credential);
-// }
